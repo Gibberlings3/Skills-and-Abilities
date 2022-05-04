dc77c6b5b1cd0fadee9d486428dd80e56093b4e6
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-05-03 22:47:23 -0700

    Narlen Dialog

--- skills-and-abilities/dlg/narlen.d
+++ skills-and-abilities/dlg/narlen.d
@@ -6,5 +6,4 @@
 
 REPLACE_STATE_TRIGGER NARLEN 0 
 ~Global("TalkedToNarlen","GLOBAL",0)
-!IfValidForPartyDialog([0.0.0.THIEF_ALL])
-!IfValidForPartyDialog([0.0.0.BARD_ALL])~ 
+!IfValidForPartyDialog([0.0.0.THIEF_ALL])~ 
--- skills-and-abilities/dlg/narlenb.d
+++ skills-and-abilities/dlg/narlenb.d
@@ -10,3 +10,10 @@ REPLACE_TRIGGER_TEXT NARLEN
 ~Global("TalkedToNarlen","GLOBAL",0)
 !IfValidForPartyDialog([0.0.0.THIEF_ALL])
 !IfValidForPartyDialog([0.0.0.BARD_ALL])~  
+
+REPLACE_TRIGGER_TEXT NARLEN 
+~Global("TalkedToNarlen","GLOBAL",0)
+!InParty([0.0.0.THIEF_ALL])~
+~Global("TalkedToNarlen","GLOBAL",0)
+!InParty([0.0.0.THIEF_ALL])
+!InParty([0.0.0.BARD_ALL])~ 
