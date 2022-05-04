8d63c35447a1f8aac7aa1d7323ff3c5087449029
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-05-04 14:13:00 -0700

    Update narlenb.d

--- skills-and-abilities/dlg/narlenb.d
+++ skills-and-abilities/dlg/narlenb.d
@@ -14,7 +14,7 @@ REPLACE_TRIGGER_TEXT ~narlen~
 */  
 
 //REPLACE_TRIGGER_TEXT NARLEN ~!InParty(\[0\.0\.0\.THIEF_ALL\])~ ~!InParty([0.0.0.THIEF_ALL]) !InParty([0.0.0.BARD_ALL])~
-REPLACE_TRIGGER_TEXT NARLEN ~(Global("TalkedToNarlen","GLOBAL",0)[ %TAB%%LNL%%MNL%%WNL%]*!I~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) !I~
+REPLACE_TRIGGER_TEXT NARLEN ~(Global("TalkedToNarlen","GLOBAL",0)[ %TAB%%LNL%%MNL%%WNL%]+!I~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) !I~
 //REPLACE_TRIGGER_TEXT NARLEN ~Global("TalkedToNarlen","GLOBAL",0)\([ %TAB%%LNL%%MNL%%WNL%!]*[ %TAB%%LNL%%MNL%%WNL%!]*\)!~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) \1~
 REPLACE_TRIGGER_TEXT NARLEN ~!IfValidForPartyDialog(\[0\.0\.0\.THIEF_ALL\])~ ~!IfValidForPartyDialog([0.0.0.THIEF_ALL]) !IfValidForPartyDialog([0.0.0.BARD_ALL])~  
 REPLACE_TRIGGER_TEXT NARLEN ~!See(\[PC\.0\.0\.BARD\])~ ~~
