2f095dc173771f44f05fcee962b88303a6605c57
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-05-04 10:55:27 -0700

    Update narlenb.d

--- skills-and-abilities/dlg/narlenb.d
+++ skills-and-abilities/dlg/narlenb.d
@@ -13,6 +13,7 @@ REPLACE_TRIGGER_TEXT ~narlen~
 !IfValidForPartyDialog([0.0.0.BARD_ALL])~
 */  
 
-REPLACE_TRIGGER_TEXT NARLEN ~!InParty(\[0\.0\.0\.THIEF_ALL\])~ ~!InParty([0.0.0.THIEF_ALL]) !InParty([0.0.0.BARD_ALL])~
+//REPLACE_TRIGGER_TEXT NARLEN ~!InParty(\[0\.0\.0\.THIEF_ALL\])~ ~!InParty([0.0.0.THIEF_ALL]) !InParty([0.0.0.BARD_ALL])~
+REPLACE_TRIGGER_TEXT NARLEN ~Global("TalkedToNarlen","GLOBAL",0) !~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) !~
 REPLACE_TRIGGER_TEXT NARLEN ~!IfValidForPartyDialog(\[0\.0\.0\.THIEF_ALL\])~ ~!IfValidForPartyDialog([0.0.0.THIEF_ALL]) !IfValidForPartyDialog([0.0.0.BARD_ALL])~  
 REPLACE_TRIGGER_TEXT NARLEN ~!See(\[PC\.0\.0\.BARD\])~ ~~
