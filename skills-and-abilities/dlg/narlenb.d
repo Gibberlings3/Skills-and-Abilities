745d7bb1261981f1f7f14ec2833e362a3df5a5f7
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-05-04 17:34:57 -0700

    Update narlenb.d

--- skills-and-abilities/dlg/narlenb.d
+++ skills-and-abilities/dlg/narlenb.d
@@ -4,8 +4,5 @@
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-//REPLACE_TRIGGER_TEXT NARLEN ~!InParty(\[0\.0\.0\.THIEF_ALL\])~ ~!InParty([0.0.0.THIEF_ALL]) !InParty([0.0.0.BARD_ALL])~
 REPLACE_TRIGGER_TEXT NARLEN ~Global("TalkedToNarlen","GLOBAL",0)[ %TAB%%WNL%]*!~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) !~
-//REPLACE_TRIGGER_TEXT NARLEN ~Global("TalkedToNarlen","GLOBAL",0)\([ %TAB%%LNL%%MNL%%WNL%!]*[ %TAB%%LNL%%MNL%%WNL%!]*\)!~ ~Global("TalkedToNarlen","GLOBAL",0) !InParty([0.0.0.BARD_ALL]) \1~
-//REPLACE_TRIGGER_TEXT NARLEN ~!IfValidForPartyDialog(\[0\.0\.0\.THIEF_ALL\])~ ~!IfValidForPartyDialog([0.0.0.THIEF_ALL]) !IfValidForPartyDialog([0.0.0.BARD_ALL])~  
 REPLACE_TRIGGER_TEXT NARLEN ~!See(\[PC\.0\.0\.BARD\])~ ~~
