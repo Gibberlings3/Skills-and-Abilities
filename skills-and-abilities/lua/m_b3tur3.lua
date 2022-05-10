24a0e1c1a261ac7bc9f444af95d43c27a690bfd5
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-05-09 21:56:16 -0700

    Update m_b3tur3.lua

--- skills-and-abilities/lua/m_b3tur3.lua
+++ skills-and-abilities/lua/m_b3tur3.lua
@@ -1,6 +1,6 @@
 function B3RemoveTurnUndeadClericActionbarListener(config, state)
    if state == 3 or state == 8 or state == 14 or state == 17 then
-      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.QUICK_SPELL_3)
    end
 end
 EEex_Actionbar_AddListener(B3RemoveTurnUndeadClericActionbarListener)
