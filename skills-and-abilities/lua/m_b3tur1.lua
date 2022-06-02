a226604ce7029daa29826397d8310fe46f3f00f4
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-06-02 14:03:07 -0700

    Update m_b3tur1.lua

--- skills-and-abilities/lua/m_b3tur1.lua
+++ skills-and-abilities/lua/m_b3tur1.lua
@@ -1,7 +1,7 @@
 function B3RemoveTurnUndeadPaladinActionbarListener(config, state)
    if  
       state == 6 
-      and not EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4005
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit ~= 0x4005
       then
       EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
    end
