34007cc6cf6f370d8e438510f910b98b766f7bd1
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-11-11 15:58:39 -0700

    Update m_b3ct01.lua

--- skills-and-abilities/lua/m_b3ct01.lua
+++ skills-and-abilities/lua/m_b3ct01.lua
@@ -1,6 +1,6 @@
 function B3ClericThiefActionbarListener(config, state)
    if state == 15 then
-      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.THIEVING)
+      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.THIEVING)
    end
 end
 EEex_Actionbar_AddListener(B3ClericThiefActionbarListener)
