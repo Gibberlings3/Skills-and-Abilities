a9f86bd11f620a9e09923b63731c357b93f81439
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-11-11 14:21:52 -0700

    Cleric/THIEF not mage....

--- skills-and-abilities/lua/m_b3cm01.lua
+++
@@ -1,6 +0,0 @@
-function B3ClericMageActionbarListener(config, state)
-   if state == 14 then
-      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.THIEVING)
-   end
-end
-EEex_Actionbar_AddListener(B3ClericMageActionbarListener)
