da8226ede4aa64295cc23c9a5b32c76980d0be30
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-11-12 21:42:28 -0700

    Update Spell Interruptions

--- skills-and-abilities/lua/m_b3test.lua
+++ skills-and-abilities/lua/m_b3test.lua
@@ -1,13 +1,22 @@
 function B3Test(sprite)
-    local damageCheck = sprite.m_tookDamage + 10
-    local rollToResist = Infinity_RandomNumber(0, 20) + sprite:getStat(32) + ((sprite.getStat(41) / 2) - 5)
+    local damageCheck = sprite.m_triggerID(0x40CD) + 10 -- Damage Taken +10
+    --local damageCheck = sprite.m_nLastDamageTaken + 10 -- Damage Taken + 10
+    local diceRoll = Infinity_RandomNumber(1, 20) -- 1d20
+    local luckBonus = sprite:getStat(32) -- Luck Bonus
+    local conBonus = (sprite:getStat(41) / 2) - 5 -- (Constitution / 2) - 5
+    --local rollToResist = Infinity_RandomNumber(1, 20) + sprite:getStat(32) + ((sprite:getStat(41) / 2) - 5)
     local concentrationProf = sprite:getStat(96) -- Concentration Prof
+    local rollToResist = 0
+    local profBonus = 0
     if concentrationProf == 1 then
-        rollToResist = rollToResist + 4
+        profBonus = 4
     elseif concentrationProf == 2 then
-        rollToResist = rollToResist + 8
+        profBonus = 8
     end
-    Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d): %s",
-        sprite:getName(), damageCheck, rollToResist, damageCheck >= rollToResist and "disrupted" or "resisted"))
+
+    local rollToResist = diceRoll + luckBonus + conBonus + profBonus
+
+    Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d) = diceRoll(%d) + luckBonus(%d) + conBonus(%d) + profBonus(%d): %s",
+        sprite:getName(), damageCheck, rollToResist, diceRoll, luckBonus, conBonus, profBonus, damageCheck >= rollToResist and "disrupted" or "resisted"))
     return damageCheck >= rollToResist
 end
