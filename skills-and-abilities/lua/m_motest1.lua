f167d16f1fb8dacf28b12eae20264f8aacb22c0a
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-08-20 16:37:11 -0700

    Update lua file

--- skills-and-abilities/lua/m_motest1.lua
+++ skills-and-abilities/lua/m_motest1.lua
@@ -10,7 +10,17 @@ function MoTest(sprite, damageData)
     end
     local rollToResist = diceRoll + luckBonus + conBonus
 
-    Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d) = diceRoll(%d) + luckBonus(%d) + conBonus(%d): %s",
-        sprite:getName(), damageCheck, rollToResist, diceRoll, luckBonus, conBonus, damageCheck >= rollToResist and "disrupted" or "resisted"))
-    return damageCheck >= rollToResist
+    if diceRoll == 1 then
+        Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d) = diceRoll(%d) + luckBonus(%d) + conBonus(%d): Critical Failure",
+            sprite:getName(), damageCheck, rollToResist, diceRoll, luckBonus, conBonus))
+        return true
+    elseif diceRoll == 20 then
+        Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d) = diceRoll(%d) + luckBonus(%d) + conBonus(%d): Critical Success",
+            sprite:getName(), damageCheck, rollToResist, diceRoll, luckBonus, conBonus))
+        return false
+    elseif diceRoll > 1 and diceRoll < 20 then
+        Infinity_DisplayString(string.format("%s checking spell disruption - damageCheck(%d) vs rollToResist(%d) = diceRoll(%d) + luckBonus(%d) + conBonus(%d): %s",
+            sprite:getName(), damageCheck, rollToResist, diceRoll, luckBonus, conBonus, damageCheck >= rollToResist and "disrupted" or "resisted"))
+        return damageCheck >= rollToResist
+    end
 end
